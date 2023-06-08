83e3ae9a77cde67b807e5730a848368532d7dd93
firefather <firefather@telenet.be>
2023-06-08 17:59:09 +0200

    update binary & nnue

--- src/nnue/misc.cpp
+++ src/nnue/misc.cpp
@@ -1,20 +1,20 @@
 /*
-  Fire is a freeware UCI chess playing engine authored by Norman Schmidt.
-
-  Fire utilizes many state-of-the-art chess programming ideas and techniques
-  which have been documented in detail at https://www.chessprogramming.org/
-  and demonstrated via the very strong open-source chess engine Stockfish...
-  https://github.com/official-stockfish/Stockfish.
-
-  Fire is free software: you can redistribute it and/or modify it under the
-  terms of the GNU General Public License as published by the Free Software
-  Foundation, either version 3 of the License, or any later version.
-
-  You should have received a copy of the GNU General Public License with
-  this program: copying.txt.  If not, see <http://www.gnu.org/licenses/>.
-
   Thanks to Yu Nasu, Hisayori Noda. This implementation adapted from R. De Man
   and Daniel Shaw's Cfish nnue probe code https://github.com/dshawul/nnue-probe
+
+  modified as follows via clang cpp core guidelines:
+  declaration and assignment joined
+  deprecated C++ headers replaced [modernize-deprecated-headers]
+  macros used to declare a constant changed to 'constexpr' constant [cppcoreguidelines-macro-usage]
+  parameters made const
+  c-style casts replaced with C++ casts
+  name does not match rule 'Global constants'
+  name does not match rule 'Enum members'
+  auto used when initializing with a cast to avoid duplicating the type name [modernize-use-auto]
+  zero constants replaced with nullptr
+  local variables made const
+  cast from 'const char *' to 'char *' dropped const qualifier [clang-diagnostic-cast-qual]
+  redundant C-style casts removed
 */
 
 #include <fcntl.h>
